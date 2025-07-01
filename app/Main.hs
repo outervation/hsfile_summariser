6452fc5937c476385bb8485c6d27dde0acfeacae
Jonathan Barnard <jonathan.barnard@outervation.ai>
2025-07-01 23:36:00 +0200

    ormolu it

--- app/Main.hs
+++ app/Main.hs
@@ -1,42 +1,43 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE TypeFamilies        #-}
 
 module Main (main) where
 
-import           Control.Monad
-import           Control.Monad.State.Strict
-import qualified Data.Set                   as S
-import           Data.Generics.Uniplate.Data       -- generic traversal
+import Control.Monad
+import Control.Monad.State.Strict
+import Data.Generics.Uniplate.Data -- generic traversal
+import Data.Set qualified as S
 import GHC (ParsedSource, runGhc)
-import           GHC.Hs
-import           GHC.Hs.Expr ()
-import           GHC.Parser ()
-import GHC.Utils.Panic (panic)
-import           GHC.Parser.Lexer ()
-import           GHC.Types.Name.Reader        (mkVarUnqual, RdrName)
-import           GHC.Types.SrcLoc (combineSrcSpans
-                                   , GenLocated(..)
-                                   , SrcSpan(..)
-                                   , RealSrcSpan
-                                   , isGoodSrcSpan
-                                   , noSrcSpan)
-import           GHC.Utils.Outputable         (showSDocUnsafe, ppr)
-import           Language.Haskell.GHC.ExactPrint.Parsers
-import           Language.Haskell.GHC.ExactPrint         (exactPrint)
-import Language.Haskell.Syntax.Expr ()
-import Language.Haskell.Syntax.Extension  ()
+import GHC.Data.FastString (fsLit)
+import GHC.Hs
 import GHC.Hs.Binds ()
+import GHC.Hs.Expr ()
 import GHC.Hs.Extension ()
-import GHC.Paths (libdir)
-import           System.Environment                 (getArgs)
-import           System.Exit                       (die)
-import           GHC.Data.FastString          (fsLit)              
+import GHC.Parser ()
 import GHC.Parser.Annotation ()
+import GHC.Parser.Lexer ()
+import GHC.Paths (libdir)
+import GHC.Types.Name.Reader (RdrName, mkVarUnqual)
+import GHC.Types.SrcLoc
+  ( GenLocated (..),
+    RealSrcSpan,
+    SrcSpan (..),
+    combineSrcSpans,
+    isGoodSrcSpan,
+    noSrcSpan,
+  )
+import GHC.Utils.Outputable (ppr, showSDocUnsafe)
+import GHC.Utils.Panic (panic)
+import Language.Haskell.GHC.ExactPrint (exactPrint)
+import Language.Haskell.GHC.ExactPrint.Parsers
+import Language.Haskell.Syntax.Expr ()
+import Language.Haskell.Syntax.Extension ()
+import System.Environment (getArgs)
+import System.Exit (die)
 
 -- | RHS/source-location information we want to blank out
 type BodySpans = S.Set RealSrcSpan
@@ -46,9 +47,10 @@ type BodySpans = S.Set RealSrcSpan
 ----------------------------------------------------------------------
 
 getOneArg :: IO FilePath
-getOneArg = getArgs >>= \case
-  [p] -> pure p
-  _   -> die "Usage: hsfile-summariser <file.hs>"
+getOneArg =
+  getArgs >>= \case
+    [p] -> pure p
+    _ -> die "Usage: hsfile-summariser <file.hs>"
 
 (|>) :: a -> (a -> b) -> b
 (|>) = flip ($)
@@ -64,21 +66,20 @@ assertRealSpan x = case x of
 
 lgrhssSpan :: LGRHS GhcPs (LHsExpr GhcPs) -> SrcSpan
 lgrhssSpan lgrhs = getHasLoc lgrhs
-  
 
 -- A robust helper to combine a list of SrcSpans into one.
 -- It handles empty lists and invalid spans gracefully.
 combineSpans :: [SrcSpan] -> SrcSpan
 combineSpans spans =
   case filter isGoodSrcSpan spans of
-    []     -> noSrcSpan
-    (s:ss) -> foldl' combineSrcSpans s ss
+    [] -> noSrcSpan
+    (s : ss) -> foldl' combineSrcSpans s ss
 
 getLocalBindsLocation :: HsLocalBinds GhcPs -> SrcSpan
 getLocalBindsLocation (HsValBinds _ (ValBinds _ binds sigs)) =
   let bind_spans = map getHasLoc binds
-      sig_spans  = map getHasLoc sigs
-  in combineSpans (bind_spans ++ sig_spans)
+      sig_spans = map getHasLoc sigs
+   in combineSpans (bind_spans ++ sig_spans)
 getLocalBindsLocation (HsIPBinds _ (IPBinds _ ip_binds)) =
   combineSpans (map getHasLoc ip_binds)
 getLocalBindsLocation (EmptyLocalBinds _) = noSrcSpan
@@ -95,70 +96,70 @@ grhssSpan' (GRHSs {..}) =
   -- Get the spans of all the individual guarded expressions.
   let grhs_spans = map lgrhssSpan grhssGRHSs
 
-  -- Get the span of the 'where' clause.
+      -- Get the span of the 'where' clause.
       binds_span = getLocalBindsLocation grhssLocalBinds
-
-  -- Combine them all into a single span.
-   in combineSpans (binds_span : grhs_spans)
+   in -- Combine them all into a single span.
+      combineSpans (binds_span : grhs_spans)
 
 grhssSpan :: GRHSs GhcPs (LHsExpr GhcPs) -> RealSrcSpan
 grhssSpan x = assertRealSpan $ grhssSpan' x
-                              
+
 ----------------------------------------------------------------------
 -- 2  Traverse & strip
 ----------------------------------------------------------------------
 
-stripModule
-  :: GHC.ParsedSource -> State BodySpans GHC.ParsedSource
+stripModule ::
+  GHC.ParsedSource -> State BodySpans GHC.ParsedSource
 stripModule = transformBiM stripBind
 
-stripBind
-  :: LHsDecl GhcPs
-  -> State BodySpans (LHsDecl GhcPs)
+stripBind ::
+  LHsDecl GhcPs ->
+  State BodySpans (LHsDecl GhcPs)
 stripBind (L l (ValD ext bind)) =
   case bind of
     -- Note: we pattern match to get all fields of FunBind
-    f@FunBind{..} -> do
+    f@FunBind {..} -> do
       mg' <- stripMatchGroup fun_matches
       -- Reconstruct the FunBind explicitly. This is safe and total.
-      let new_bind = f { fun_matches = mg' }
+      let new_bind = f {fun_matches = mg'}
       pure $ L l (ValD ext new_bind)
 
     -- And we do the same for PatBind
-    p@PatBind{..} -> do
+    p@PatBind {..} -> do
       let rhs0 = pat_rhs
       rhs' <- blankGRHSs rhs0
       modify (S.insert (grhssSpan rhs0))
       -- Reconstruct the PatBind explicitly.
-      let new_bind = p { pat_rhs = rhs' }
+      let new_bind = p {pat_rhs = rhs'}
       pure $ L l (ValD ext new_bind)
 
     -- Other bind types are left alone
     _ -> pure (L l (ValD ext bind))
-
-stripBind d = pure d   -- type sigs etc.
+stripBind d = pure d -- type sigs etc.
 
 -- | Replace every GRHS list with a single ‘<<hidden>>’ expression
-stripMatchGroup
-  :: MatchGroup GhcPs (LHsExpr GhcPs)
-  -> State BodySpans (MatchGroup GhcPs (LHsExpr GhcPs))
-stripMatchGroup mg@(MG { mg_alts = L _ alts }) = do
+stripMatchGroup ::
+  MatchGroup GhcPs (LHsExpr GhcPs) ->
+  State BodySpans (MatchGroup GhcPs (LHsExpr GhcPs))
+stripMatchGroup mg@(MG {mg_alts = L _ alts}) = do
   alts' <- forM alts $ \(L l m) -> do
     rhs' <- blankGRHSs (m_grhss m)
-    pure (L l m { m_grhss = rhs' })
-  pure mg { mg_alts = noLocA alts' }
+    pure (L l m {m_grhss = rhs'})
+  pure mg {mg_alts = noLocA alts'}
 
-blankGRHSs
-  :: GRHSs GhcPs (LHsExpr GhcPs)
-  -> State BodySpans (GRHSs GhcPs (LHsExpr GhcPs))
+blankGRHSs ::
+  GRHSs GhcPs (LHsExpr GhcPs) ->
+  State BodySpans (GRHSs GhcPs (LHsExpr GhcPs))
 blankGRHSs grhss = do
   -- remember span of the body for comment filtering
   modify (S.insert (grhssSpan grhss))
   let lhs = noLocA $ HsVar noExtField (noLocA identHidden)
-      gr  = noLocA $ GRHS  noAnn [] lhs
-  pure grhss { grhssGRHSs = [gr]         -- replace bodies
-             , grhssLocalBinds = GHC.Hs.emptyLocalBinds
-             }
+      gr = noLocA $ GRHS noAnn [] lhs
+  pure
+    grhss
+      { grhssGRHSs = [gr], -- replace bodies
+        grhssLocalBinds = GHC.Hs.emptyLocalBinds
+      }
 
 identHidden :: RdrName
 identHidden = mkVarUnqual (fsLit " = <<hidden>>")
@@ -173,20 +174,21 @@ main = do
   -- 'runGhc' initializes a GHC session and executes a Ghc monad action,
   -- returning the result into the IO monad.
   parsed0 <- runGhc (Just libdir) $ do
-      r <- liftIO $ parseModule libdir file
-      case r of
-        Left errs -> liftIO $ die (showSDocUnsafe $ ppr errs)
-        Right ps -> pure ps
+    r <- liftIO $ parseModule libdir file
+    case r of
+      Left errs -> liftIO $ die (showSDocUnsafe $ ppr errs)
+      Right ps -> pure ps
 
   -- Once runGhc is complete, we are back in the IO monad with the results.
   let (parsed, _) = runState (stripModule parsed0) S.empty
-      result          = exactPrint parsed 
-                         |> condenseBlanks
+      result =
+        exactPrint parsed
+          |> condenseBlanks
   putStrLn result
-  
+
 condenseBlanks :: String -> String
 condenseBlanks = unlines . go . lines
   where
-    go (a:b:rest) | all null [a,b] = go ("" : rest)
-    go (x:rest)                    = x : go rest
-    go []                          = []
+    go (a : b : rest) | all null [a, b] = go ("" : rest)
+    go (x : rest) = x : go rest
+    go [] = []
